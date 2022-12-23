8c60d59a446335781b63fc413210665c83547cc1
Jake O'Shannessy <joshannessy@affinity-fire.com>
2022-12-23 17:33:53 +0000

    Display error locations better

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+use clap::{crate_name, crate_version, value_parser, Arg, ArgMatches, Command};
+use namelist::{
+    tokenizer::{LocatedToken, Span, Token, TokenizerError},
+    Namelist,
+};
 use std::{
     collections::HashMap,
     io::{Read, Write},
     slice::Iter,
-};
-
-use clap::{crate_name, crate_version, value_parser, Arg, Command};
-use namelist::{
-    tokenizer::{LocatedToken, Token},
-    Namelist,
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -37,17 +36,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .help("Path to the input file or '-' for stdin"),
         )
         .get_matches();
-    let mut input_handle: Box<dyn Read> = {
-        let file_path = matches
-            .get_one::<String>("INPUT-PATH")
-            .expect("No input path");
+    let file_path = matches
+        .get_one::<String>("INPUT-PATH")
+        .expect("No input path");
+    let input_handle: Box<dyn Read> = {
         if file_path == "-" {
             Box::new(std::io::stdin())
         } else {
             Box::new(std::fs::File::open(file_path)?)
         }
     };
-    let mut output_handle: Box<dyn Write> = {
+    let output_handle: Box<dyn Write> = {
         let file_path = matches
             .get_one::<String>("OUTPUT-PATH")
             .expect("No output path");
@@ -57,106 +56,219 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             Box::new(std::fs::File::create(file_path)?)
         }
     };
-    let input = {
-        let mut buf = String::new();
-        input_handle.read_to_string(&mut buf)?;
-        buf
-    };
-    let parser = namelist::NmlParser::new(std::io::Cursor::new(input));
-    let mut nmls: Vec<_> = parser.collect();
-    if let Some(n_mpi) = matches.get_one::<usize>("N-PROCESSES").copied() {
-        allocate_mpi_processes(&mut nmls, n_mpi);
+    match run(&matches, input_handle, output_handle) {
+        Ok(_) => Ok(()),
+        Err(FdsParseError::Io(err)) => Err(Box::new(err)),
+        Err(err) => {
+            if let Some(span) = err.span() {
+                eprintln!(
+                    "ERROR: {}:{}:{} {err}",
+                    file_path,
+                    span.line + 1,
+                    span.column + 1
+                );
+            } else {
+                eprintln!("ERROR: {} {err}", file_path);
+            }
+            std::process::exit(1);
+        }
     }
-    for nml in nmls.into_iter() {
-        output_handle.write_all(nml.to_string().as_bytes())?;
-    }
+}
 
+fn run(
+    matches: &ArgMatches,
+    input_handle: impl Read,
+    output_handle: impl Write,
+) -> Result<(), FdsParseError> {
+    let mut fds_file = FdsFile::from_reader(input_handle)?;
+    // If the number of MPI processes has been nominated, reallocate meshes.
+    if let Some(n_mpi) = matches.get_one::<usize>("N-PROCESSES").copied() {
+        fds_file.allocate_mpi_processes(n_mpi)?;
+    }
+    fds_file
+        .write_all(output_handle)
+        .map_err(FdsParseError::Io)?;
     Ok(())
 }
 
-fn allocate_mpi_processes(nmls: &mut [Namelist], n_mpi: usize) {
-    let mut meshes = HashMap::new();
-    {
-        // Count cells
-        for (i, nml) in nmls.iter().enumerate() {
-            if nml.tokens().get(1).map(|x| &x.token) == Some(&Token::Identifier("MESH".to_string()))
-            {
-                let pnml = parse_namelist(nml).unwrap();
-                let n_cells = count_mesh_cells(&pnml).unwrap();
-                meshes.insert(i, n_cells);
+#[derive(Debug)]
+pub enum FdsParseError {
+    Tokenize(TokenizerError),
+    Parse(Option<Span>, String),
+    Io(std::io::Error),
+}
+
+impl FdsParseError {
+    pub fn span(&self) -> Option<Span> {
+        match self {
+            Self::Tokenize(err) => Some(err.span()),
+            Self::Parse(span, _) => *span,
+            Self::Io(_) => None,
+        }
+    }
+}
+
+impl std::fmt::Display for FdsParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Tokenize(err) => {
+                write!(f, "{err}")
+            }
+            Self::Parse(_, err) => {
+                write!(f, "{err}")
+            }
+            Self::Io(err) => {
+                write!(f, "{err}")
             }
         }
     }
-    let mut meshes: Vec<_> = meshes.into_iter().collect();
-    meshes.sort_by(|a, b| a.1.cmp(&b.1));
-    meshes.reverse();
-    let mut buckets: Vec<Vec<(usize, usize)>> = vec![vec![]; n_mpi];
-    for mesh in &meshes {
-        // get the minimum bucket
-        let min_bucket = buckets
-            .iter_mut()
-            .min_by(|a, b| {
-                let n_a = a.iter().map(|(_, n)| n).sum::<usize>();
-                let n_b = b.iter().map(|(_, n)| n).sum::<usize>();
-                n_a.cmp(&n_b)
-            })
-            .unwrap();
-        min_bucket.push(*mesh);
-    }
-    let mut mesh_process = HashMap::new();
-    for (i, bucket) in buckets.iter().enumerate() {
-        for (mesh_num, _) in bucket.iter() {
-            mesh_process.insert(mesh_num, i);
+}
+
+impl std::error::Error for FdsParseError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Tokenize(err) => Some(err),
+            Self::Parse(_, _) => None,
+            Self::Io(err) => Some(err),
         }
     }
-    eprintln!("MPI Mesh Allocation");
-    for (i, bucket) in buckets.iter().enumerate() {
-        let total_cells = bucket.iter().map(|(_, n)| n).sum::<usize>();
-        let cells: Vec<_> = bucket.iter().map(|(_, n)| n).collect();
-        eprintln!("  MPI_PROCESS {i}: TOTAL: {total_cells} {cells:?}");
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct FdsFile {
+    pub nmls: Vec<Namelist>,
+}
+
+impl FdsFile {
+    pub fn new(nmls: Vec<Namelist>) -> Self {
+        Self { nmls }
     }
-    let process_cells: Vec<usize> = buckets
-        .iter()
-        .map(|buckets| buckets.iter().map(|(_, n)| n).sum::<usize>())
-        .collect();
-    let min_bucket = *process_cells.iter().min().unwrap() as f64;
-    let max_bucket = *process_cells.iter().max().unwrap() as f64;
-    let variation = ((max_bucket - min_bucket) / 2.0) / ((max_bucket + min_bucket) / 2.0) * 100.0;
-    eprintln!("MPI Cell Count Variation: +/- {:.2}", variation);
-    let mut old_mesh_locations: Vec<usize> = meshes.iter().map(|(i, _)| *i).collect();
-    old_mesh_locations.sort();
-    for (i, nml) in nmls.iter_mut().enumerate() {
-        if nml.tokens().get(1).map(|x| &x.token) == Some(&Token::Identifier("MESH".to_string())) {
-            let process_num = mesh_process.get(&i).unwrap();
-            nml.append_token(Token::Identifier("MPI_PROCESS".to_string()));
-            nml.append_token(Token::Equals);
-            nml.append_token(Token::Number(format!("{process_num}")));
-            nml.append_token(Token::Whitespace(" ".to_string()));
+
+    pub fn from_reader(mut input_handle: impl Read) -> Result<Self, FdsParseError> {
+        let input = {
+            let mut buf = String::new();
+            input_handle
+                .read_to_string(&mut buf)
+                .map_err(FdsParseError::Io)?;
+            buf
+        };
+        let parser = namelist::NmlParser::new(std::io::Cursor::new(input));
+        let nmls: Vec<_> = parser
+            .collect::<Result<Vec<_>, TokenizerError>>()
+            .map_err(FdsParseError::Tokenize)?;
+        Ok(Self { nmls })
+    }
+
+    pub fn write_all(&self, mut output_handle: impl Write) -> std::io::Result<()> {
+        for nml in self.nmls.iter() {
+            output_handle.write_all(nml.to_string().as_bytes())?;
         }
+        Ok(())
     }
-    let mut new_meshes = vec![];
-    for &old_location in old_mesh_locations.iter() {
-        let mut mesh: Namelist = Namelist::Other { tokens: vec![] };
-        std::mem::swap(&mut mesh, nmls.get_mut(old_location).unwrap());
-        new_meshes.push((mesh_process.get(&old_location).unwrap(), mesh));
+
+    /// Count the number of cells in the model.
+    pub fn n_cells(&self) -> Option<usize> {
+        let mut n = 0;
+        for nml in self.nmls.iter() {
+            if nml.tokens().get(1).map(|x| &x.token) == Some(&Token::Identifier("MESH".to_string()))
+            {
+                let pnml = parse_namelist(nml).unwrap();
+                n += count_mesh_cells(&pnml).unwrap();
+            }
+        }
+        Some(n)
     }
-    new_meshes.sort_by(|a, b| a.0.cmp(b.0));
-    // TODO: Run a very inefficient selection sort.
-    for (old_location, (_, mut nml)) in old_mesh_locations.iter().zip(new_meshes.into_iter()) {
-        std::mem::swap(&mut nml, nmls.get_mut(*old_location).unwrap());
+
+    pub fn allocate_mpi_processes(&mut self, n_mpi: usize) -> Result<(), FdsParseError> {
+        let nmls = &mut self.nmls;
+        let mut meshes = HashMap::new();
+        {
+            // Count cells
+            for (i, nml) in nmls.iter().enumerate() {
+                if nml.tokens().get(1).map(|x| &x.token)
+                    == Some(&Token::Identifier("MESH".to_string()))
+                {
+                    let pnml = parse_namelist(nml).unwrap();
+                    let n_cells = count_mesh_cells(&pnml)?;
+                    meshes.insert(i, n_cells);
+                }
+            }
+        }
+        let mut meshes: Vec<_> = meshes.into_iter().collect();
+        meshes.sort_by(|a, b| a.1.cmp(&b.1));
+        meshes.reverse();
+        let mut buckets: Vec<Vec<(usize, usize)>> = vec![vec![]; n_mpi];
+        for mesh in &meshes {
+            // get the minimum bucket
+            let min_bucket = buckets
+                .iter_mut()
+                .min_by(|a, b| {
+                    let n_a = a.iter().map(|(_, n)| n).sum::<usize>();
+                    let n_b = b.iter().map(|(_, n)| n).sum::<usize>();
+                    n_a.cmp(&n_b)
+                })
+                .unwrap();
+            min_bucket.push(*mesh);
+        }
+        let mut mesh_process = HashMap::new();
+        for (i, bucket) in buckets.iter().enumerate() {
+            for (mesh_num, _) in bucket.iter() {
+                mesh_process.insert(mesh_num, i);
+            }
+        }
+        eprintln!("MPI Mesh Allocation");
+        for (i, bucket) in buckets.iter().enumerate() {
+            let total_cells = bucket.iter().map(|(_, n)| n).sum::<usize>();
+            let cells: Vec<_> = bucket.iter().map(|(_, n)| n).collect();
+            eprintln!("  MPI_PROCESS {i}: TOTAL: {total_cells} {cells:?}");
+        }
+        let process_cells: Vec<usize> = buckets
+            .iter()
+            .map(|buckets| buckets.iter().map(|(_, n)| n).sum::<usize>())
+            .collect();
+        let min_bucket = *process_cells.iter().min().unwrap() as f64;
+        let max_bucket = *process_cells.iter().max().unwrap() as f64;
+        let variation =
+            ((max_bucket - min_bucket) / 2.0) / ((max_bucket + min_bucket) / 2.0) * 100.0;
+        eprintln!("MPI Cell Count Variation: +/- {:.2}", variation);
+        let mut old_mesh_locations: Vec<usize> = meshes.iter().map(|(i, _)| *i).collect();
+        old_mesh_locations.sort();
+        for (i, nml) in nmls.iter_mut().enumerate() {
+            if nml.tokens().get(1).map(|x| &x.token) == Some(&Token::Identifier("MESH".to_string()))
+            {
+                let process_num = mesh_process.get(&i).unwrap();
+                nml.append_token(Token::Identifier("MPI_PROCESS".to_string()));
+                nml.append_token(Token::Equals);
+                nml.append_token(Token::Number(format!("{process_num}")));
+                nml.append_token(Token::Whitespace(" ".to_string()));
+            }
+        }
+        let mut new_meshes = vec![];
+        for &old_location in old_mesh_locations.iter() {
+            let mut mesh: Namelist = Namelist::Other { tokens: vec![] };
+            std::mem::swap(&mut mesh, nmls.get_mut(old_location).unwrap());
+            new_meshes.push((mesh_process.get(&old_location).unwrap(), mesh));
+        }
+        new_meshes.sort_by(|a, b| a.0.cmp(b.0));
+        for (old_location, (_, mut nml)) in old_mesh_locations.iter().zip(new_meshes.into_iter()) {
+            std::mem::swap(&mut nml, nmls.get_mut(*old_location).unwrap());
+        }
+        Ok(())
     }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct ParsedNamelist {
     pub group: String,
+    pub span: Option<Span>,
     pub parameters: HashMap<String, ParameterValues>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct ParameterValues {
-    pub dimensions: Vec<Token>,
-    pub values: Vec<Token>,
+    pub span: Option<Span>,
+    pub dimensions: Vec<LocatedToken>,
+    pub values: Vec<LocatedToken>,
 }
 
 fn next_non_ws<'a>(tokens: &'a mut Iter<LocatedToken>) -> Option<&'a LocatedToken> {
@@ -176,44 +288,45 @@ fn parse_namelist(nml: &namelist::Namelist) -> Option<ParsedNamelist> {
     if tokens.next()?.token != Token::Ampersand {
         return None;
     }
-    let group = if let Token::Identifier(s) = &tokens.next()?.token {
-        s.to_string()
-    } else {
-        return None;
+
+    let (group, group_span) = {
+        let gn = tokens.next()?;
+        if let Token::Identifier(s) = &gn.token {
+            (s.to_string(), gn.span())
+        } else {
+            return None;
+        }
     };
     let mut parameters: HashMap<String, ParameterValues> = Default::default();
-    let mut token_buf: Vec<Token> = vec![];
+    let mut token_buf: Vec<LocatedToken> = vec![];
     while let Some(pn) = token_buf
         .pop()
-        .as_ref()
-        .or_else(|| next_non_ws(&mut tokens).map(|lt| &lt.token))
+        .or_else(|| next_non_ws(&mut tokens).cloned())
     {
         // Take parameter name
         let parameter_name = pn.clone();
-        if parameter_name == Token::RightSlash {
+        if parameter_name.token == Token::RightSlash {
             break;
         }
-        if let Token::Identifier(name) = &parameter_name {
+        if let Token::Identifier(name) = &parameter_name.token {
             {
                 let b = token_buf.pop();
-                let token = b
-                    .as_ref()
-                    .or_else(|| next_non_ws(&mut tokens).map(|lt| &lt.token));
-                if let Some(Token::Equals) = token {
+                let token = b.as_ref().or_else(|| next_non_ws(&mut tokens));
+                if let Some(Token::Equals) = token.map(|lt| &lt.token) {
                 } else {
                     panic!("no equals: {token:?}");
                 };
             }
-            let mut value_tokens: Vec<Token> = vec![];
+            let mut value_tokens: Vec<LocatedToken> = vec![];
             while let Some(token) = token_buf
                 .pop()
                 .as_ref()
-                .or_else(|| next_non_ws(&mut tokens).map(|lt| &lt.token))
+                .or_else(|| next_non_ws(&mut tokens))
             {
-                if token == &Token::RightSlash {
+                if &token.token == &Token::RightSlash {
                     break;
                 }
-                if token == &Token::Equals {
+                if &token.token == &Token::Equals {
                     token_buf.push(token.clone());
                     if let Some(t) = value_tokens.pop() {
                         token_buf.push(t);
@@ -225,6 +338,7 @@ fn parse_namelist(nml: &namelist::Namelist) -> Option<ParsedNamelist> {
             parameters.insert(
                 name.to_string(),
                 ParameterValues {
+                    span: pn.span,
                     dimensions: vec![],
                     values: value_tokens,
                 },
@@ -235,27 +349,34 @@ fn parse_namelist(nml: &namelist::Namelist) -> Option<ParsedNamelist> {
             panic!("invalid parameter name {:?}", parameter_name);
         }
     }
-    Some(ParsedNamelist { group, parameters })
+    Some(ParsedNamelist {
+        group,
+        span: group_span,
+        parameters,
+    })
 }
 
-fn count_mesh_cells(pnml: &ParsedNamelist) -> Option<usize> {
-    let ijk = pnml.parameters.get("IJK")?;
+fn count_mesh_cells(pnml: &ParsedNamelist) -> Result<usize, FdsParseError> {
+    let ijk = pnml.parameters.get("IJK").ok_or(FdsParseError::Parse(
+        pnml.span,
+        "no IJK parameter for mesh".to_string(),
+    ))?;
     let values: Vec<usize> = ijk
         .values
         .iter()
-        .map(|v| match v {
-            Token::Number(ref s) => s.parse().unwrap(),
-            _ => panic!("invalid number"),
+        .map(|v| match v.token {
+            Token::Number(ref s) => Ok(s.parse().unwrap()),
+            _ => Err(FdsParseError::Parse(
+                v.span(),
+                "ERROR: invalid token for IJK".to_string(),
+            )),
         })
-        .collect();
-    let ijk: [usize; 3] = values.try_into().unwrap();
-    Some(ijk[0] * ijk[1] * ijk[2])
+        .collect::<Result<Vec<usize>, _>>()?;
+    let ijk: [usize; 3] = values.try_into().map_err(|_| {
+        FdsParseError::Parse(
+            None, // v.span(),
+            "incorrect number of IJK parameters".to_string(),
+        )
+    })?;
+    Ok(ijk[0] * ijk[1] * ijk[2])
 }
-// Identifier("IJK") }
-// LocatedToken { span: Span { lo: 43481, len: 1 }, token: Equals }
-// LocatedToken { span: Span { lo: 43482, len: 2 }, token: Number("36") }
-// LocatedToken { span: Span { lo: 43484, len: 1 }, token: Comma }
-// LocatedToken { span: Span { lo: 43485, len: 2 }, token: Number("52") }
-// LocatedToken { span: Span { lo: 43487, len: 1 }, token: Comma }
-// LocatedToken { span: Span { lo: 43488, len: 1 }, token: Number("8") }
-// LocatedToken { span: Span { lo: 43489, len: 1 }, token: Whitespace(" ") }
--- tests/integration.rs
+++ tests/integration.rs
@@ -2,21 +2,22 @@
 mod integration {
     #[test]
     fn parse_file_examples() {
-        let example_dirs = std::fs::read_dir("tests/Examples").unwrap();
+        let example_dirs = std::fs::read_dir("tests/Examples").expect("test parse failed");
         for example_dir in example_dirs {
-            let example_dir_path = example_dir.unwrap().path();
+            let example_dir_path = example_dir.expect("test parse failed").path();
             if example_dir_path.is_dir() {
-                let examples = std::fs::read_dir(&example_dir_path).unwrap();
+                let examples = std::fs::read_dir(&example_dir_path).expect("test parse failed");
                 for example in examples {
-                    let example_path = example.unwrap().path();
+                    let example_path = example.expect("test parse failed").path();
                     if example_path.is_file() {
                         eprintln!("{}", example_path.display());
-                        let input = std::fs::read_to_string(&example_path).unwrap();
+                        let input =
+                            std::fs::read_to_string(&example_path).expect("test parse failed");
                         let parser = namelist::NmlParser::new(std::io::Cursor::new(&input));
                         let nmls: Vec<_> = parser.collect();
                         let mut new = String::new();
                         for nml in nmls {
-                            new.push_str(&nml.to_string());
+                            new.push_str(&nml.expect("test parse failed").to_string());
                         }
                         assert_eq!(input, new);
                     }
